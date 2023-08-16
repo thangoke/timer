595ebab7f5fa4125cbfa7f5cd8c6ecbee6f8e641
thangok <thangokenguyen@gmail.com>
2023-08-17 06:37:46 +0700

    setup timelapse

--- src/component/TimerSetup.tsx
+++ src/component/TimerSetup.tsx
@@ -4,12 +4,53 @@ import AdjustDown from "./AdjustDown"
 import { useState } from "react"
 
 export default function TimerSetup() {
-    let [hour, setHour] = useState("00");
+    let [hour, setHour] = useState("03");
     let [minute, setMinute] = useState("00");
     let [second, setSecond] = useState("00");
 
     const setElement = (name: string, direction: string) => {
-        console.log(name, direction);
+        let amount = direction === "down" ? -1 : 1;
+        switch (name) {
+            case "hour": {
+                let currentHour = Number(hour);
+                if (amount <= 0 && currentHour <= 0) {
+                    break;
+                }
+                currentHour += amount;
+                if (currentHour < 10) {
+                    setHour('0' + currentHour);
+                } else {
+                    setHour('' + currentHour);
+                }
+                break;
+            }
+            case "minute": {
+                let currenMinute = Number(minute);
+                if (amount <= 0 && currenMinute <= 0) {
+                    break;
+                }
+                currenMinute += amount;
+                if (currenMinute < 60) {
+                    setMinute('0' + currenMinute);
+                } else {
+                    setMinute('' + currenMinute);
+                }
+                break;
+            }
+            case "second": {
+                let currenSecond = Number(second);
+                if (amount <= 0 && currenSecond <= 0) {
+                    break;
+                }
+                currenSecond += amount;
+                if (currenSecond < 60) {
+                    setSecond('0' + currenSecond);
+                } else {
+                    setSecond('' + currenSecond);
+                }
+                break;
+            }
+        }
     }
 
     return <div style={styles.timerSetupContainer}>
