bc08f3c597153bcf11e0970abafaaf336751f26b
thangok <thangokenguyen@gmail.com>
2023-08-18 02:06:25 +0700

    screen navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,39 @@ import './App.css';
 import TimerSetup from './component/TimerSetup';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faCoffee, faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faCoffee, faPlay, faPause, faCog } from '@fortawesome/free-solid-svg-icons'
 import Timer from './component/Timer';
+import HomeScreen from './component/HomeScreen';
 
-library.add(faCheckSquare, faCoffee, faPlay, faPause);
+library.add(faCheckSquare, faCoffee, faPlay, faPause, faCog);
 
 function App() {
-  let [currentScreen, setCurrentScreen] = useState("setup");
+  let [currentScreen, setCurrentScreen] = useState("home");
 
   let [hour, setHour] = useState("00");
   let [minute, setMinute] = useState("00");
   let [second, setSecond] = useState("15");
 
   const handleSetupClose = () => {
-    setCurrentScreen("timer");
+    setCurrentScreen("home");
     setHour(localStorage.getItem("thangoke.timer/hour") || "00");
     setMinute(localStorage.getItem("thangoke.timer/minute") || "00");
     setSecond(localStorage.getItem("thangoke.timer/second") || "15");
   }
 
+  const handleOnChangeScreen = (screen: string) => {
+    setCurrentScreen(screen);
+  }
+
   return (
     <div className="App">
+      {currentScreen === "home" && <HomeScreen setupFor="" title="Home" timeColor="white" onChangeScreen={(screen: string) => handleOnChangeScreen(screen)} />}
+
       {currentScreen === "setup" && <TimerSetup setupFor="" title="set time" timeColor="white" onClose={() => handleSetupClose()} />}
-      <div style={{ marginTop: "50px", marginBottom: "50px" }}></div>
-      {currentScreen === "setup" && <TimerSetup setupFor="alarm" title="set warning time" timeColor="yellow" onClose={() => handleSetupClose()} />}
-      <div style={{ marginTop: "50px", marginBottom: "50px" }}></div>
-      {currentScreen === "timer" && <Timer hour={hour} minute={minute} second={second} />}
+
+      {currentScreen === "setupAlarm" && <TimerSetup setupFor="alarm" title="set warning time" timeColor="yellow" onClose={() => handleSetupClose()} />}
+
+      {currentScreen === "timer" && <HomeScreen hour={hour} minute={minute} second={second} />}
     </div>
 
   );
---
+++ src/component/HomeScreen.tsx
@@ -0,0 +1,111 @@
+import StyleSheetHolder from "../type/StyleSheetHolder"
+import { useEffect, useState } from "react"
+import PlayPauseButton from "./PlayPauseButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function HomeScreen({ hour, minute, second, onChangeScreen }: any) {
+    let [currStatus, setCurrStatus] = useState("pause");
+
+    let [currHour, setCurrHour] = useState(hour);
+    let [currMinute, setCurrMinute] = useState(minute);
+    let [currSecond, setCurrSecond] = useState(second);
+
+    let [totalSeconds, setTotalSeconds] = useState(Number(currHour) * 3600 + Number(minute) * 60 + Number(second));
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log(currStatus)
+            if (currStatus !== "pause") {
+                if (totalSeconds > 0) {
+                    setTotalSeconds(totalSeconds - 1);
+                };
+
+                let remain = totalSeconds;
+
+                let h = Math.floor(remain / 3600);
+                if (h < 10) {
+                    setCurrHour('0' + h);
+                } else {
+                    setCurrHour('' + h);
+                }
+                remain -= h * 3600;
+
+                let m = Math.floor(remain / 60);
+                if (m < 10) {
+                    setCurrMinute('0' + m);
+                } else {
+                    setCurrMinute('' + m);
+                }
+                remain -= m * 60;
+
+                let s = remain;
+                if (s < 10) {
+                    setCurrSecond('0' + s);
+                } else {
+                    setCurrSecond('' + s);
+                }
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [currStatus, totalSeconds]);
+
+    const handleStatusChange = (status: string) => {
+        console.log("receive status", status);
+        setCurrStatus(status);
+    }
+
+    return <div style={styles.timerSetupContainer}>
+        <div style={styles.timerElement} onClick={() => onChangeScreen("setup")}>{currHour} : {currMinute} : {currSecond}</div>
+        <div style={styles.alarmContainer}>
+            <div style={styles.alarmButton} onClick={() => onChangeScreen("setupAlarm")}>00:00</div>
+            <div style={styles.alarmButton} onClick={() => { }}>LOCKED</div>
+            <div style={styles.alarmButton} onClick={() => { }}>LOCKED</div>
+        </div>
+        <div style={styles.timerAdjustContainer}>
+            <FontAwesomeIcon
+                icon={"cog"}
+                style={{ color: "white", fontWeight: "bold", fontSize: "100px", marginLeft: "55px", marginRight: "55px" }}
+            />
+            <PlayPauseButton onStatusChange={(status: string) => handleStatusChange(status)} />
+        </div>
+    </div>
+}
+
+const styles: StyleSheetHolder = {
+    timerSetupContainer: {
+        display: "flex",
+        flexDirection: "column",
+        background: "gray",
+    },
+    timerAdjustContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    timerElement: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: "100px",
+        borderTop: "2px solid white",
+        borderBottom: "2px solid white",
+        userSelect: "none",
+    },
+    alarmContainer: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        marginBottom: "30px",
+    },
+    alarmButton: {
+        color: "yellow",
+        fontSize: "30px",
+        marginLeft: "5px",
+        marginRight: "5px",
+        marginTop: "10px",
+        border: "1px solid",
+        borderRadius: "10%",
+        paddingLeft: "30px",
+        paddingRight: "30px",
+        userSelect: "none",
+    }
+}
