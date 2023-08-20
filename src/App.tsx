633195248ae0b3ae830052e1e7d84ba26dd153d4
thangok <thangokenguyen@gmail.com>
2023-08-21 06:38:03 +0700

    jump to timer

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ library.add(faCheckSquare, faCoffee, faPlay, faPause, faCog, faLock);
 function App() {
   let [currentScreen, setCurrentScreen] = useState("home");
 
-  let [hour, setHour] = useState("00");
-  let [minute, setMinute] = useState("00");
-  let [second, setSecond] = useState("15");
+  let [hour, setHour] = useState(localStorage.getItem(`thangoke.timer/${""}hour`) || "00");
+  let [minute, setMinute] = useState(localStorage.getItem(`thangoke.timer/${""}minute`) || "00");
+  let [second, setSecond] = useState(localStorage.getItem(`thangoke.timer/${""}second`) || "15");
 
   const handleSetupClose = () => {
     setCurrentScreen("home");
@@ -29,13 +29,13 @@ function App() {
 
   return (
     <div className="App">
-      {currentScreen === "home" && <HomeScreen setupFor="" title="Home" timeColor="white" onChangeScreen={(screen: string) => handleOnChangeScreen(screen)} />}
+      {currentScreen === "home" && <HomeScreen title="Home" timeColor="white" onChangeScreen={(screen: string) => handleOnChangeScreen(screen)} hour={hour} minute={minute} second={second} />}
 
       {currentScreen === "setup" && <TimerSetup setupFor="" title="set time" timeColor="white" onClose={() => handleSetupClose()} />}
 
       {currentScreen === "setupAlarm" && <TimerSetup setupFor="alarm" title="set warning time" timeColor="yellow" onClose={() => handleSetupClose()} />}
 
-      {currentScreen === "timer" && <HomeScreen hour={hour} minute={minute} second={second} />}
+      {currentScreen === "timer" && <Timer hour={hour} minute={minute} second={second} />}
     </div>
 
   );
--- src/component/HomeScreen.tsx
+++ src/component/HomeScreen.tsx
@@ -1,61 +1,10 @@
 import StyleSheetHolder from "../type/StyleSheetHolder"
-import { useEffect, useState } from "react"
 import PlayPauseButton from "./PlayPauseButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function HomeScreen({ hour, minute, second, onChangeScreen }: any) {
-    let [currStatus, setCurrStatus] = useState("pause");
-
-    let [currHour, setCurrHour] = useState(hour);
-    let [currMinute, setCurrMinute] = useState(minute);
-    let [currSecond, setCurrSecond] = useState(second);
-
-    let [totalSeconds, setTotalSeconds] = useState(Number(currHour) * 3600 + Number(minute) * 60 + Number(second));
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            console.log(currStatus)
-            if (currStatus !== "pause") {
-                if (totalSeconds > 0) {
-                    setTotalSeconds(totalSeconds - 1);
-                };
-
-                let remain = totalSeconds;
-
-                let h = Math.floor(remain / 3600);
-                if (h < 10) {
-                    setCurrHour('0' + h);
-                } else {
-                    setCurrHour('' + h);
-                }
-                remain -= h * 3600;
-
-                let m = Math.floor(remain / 60);
-                if (m < 10) {
-                    setCurrMinute('0' + m);
-                } else {
-                    setCurrMinute('' + m);
-                }
-                remain -= m * 60;
-
-                let s = remain;
-                if (s < 10) {
-                    setCurrSecond('0' + s);
-                } else {
-                    setCurrSecond('' + s);
-                }
-            }
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [currStatus, totalSeconds]);
-
-    const handleStatusChange = (status: string) => {
-        console.log("receive status", status);
-        setCurrStatus(status);
-    }
-
     return <div style={styles.timerSetupContainer}>
-        <div style={styles.timerElement} onClick={() => onChangeScreen("setup")}>{currHour} : {currMinute} : {currSecond}</div>
+        <div style={styles.timerElement} onClick={() => onChangeScreen("setup")}>{hour} : {minute} : {second}</div>
         <div style={styles.alarmContainer}>
             <div style={styles.alarmButton} onClick={() => onChangeScreen("setupAlarm")}>00:00:00</div>
             <div style={styles.alarmButton}>
@@ -76,7 +25,7 @@ export default function HomeScreen({ hour, minute, second, onChangeScreen }: any
                 icon={"cog"}
                 style={{ color: "white", fontWeight: "bold", fontSize: "100px", marginLeft: "55px", marginRight: "55px" }}
             /> */}
-            <PlayPauseButton onStatusChange={(status: string) => handleStatusChange(status)} />
+            <PlayPauseButton onStatusChange={() => onChangeScreen("timer")} />
         </div>
     </div>
 }
